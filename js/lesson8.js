049c5bd3bd8afcc23fbe7bbab3f3c09541d1bcd3
Victoria <123vika@gmail.com>
2021-05-01 18:27:20 +0300

    home work

--- js/lesson8.js
+++ js/lesson8.js
@@ -2,29 +2,29 @@
 
 // 2. Не выполняя кода, ответить, что выведет браузер и почему:
 
-if (!("a" in window)) {    
-    var a = 1;
-}
-alert(a);  // ищет функцию без аргументов, и выводит её: function a() {alert(this);}
+// if (!("a" in window)) {    
+//     var a = 1;
+// }
+// alert(a);  // undefined не определена за пределами условия
     
-var b = function a(x) {
-    x && a(--x);  // выдаст ошибку
-};
-alert(a); // как и первый alert ищет функцию без аргументов, и выводит её: function a() {alert(this);}
+// var b = function a(x) {
+//     x && a(--x);  
+// };
+// alert(a); // выдаст ошибку a не определена(и не возвращает ничего)
 
-function a(x) {
-    return x * 2;
-}
-var a;
-alert(a);// то же самое: function a() {alert(this);}
+// function a(x) {
+//     return x * 2;
+// }
+// var a;
+// alert(a);// вернёт функцию как объект function a(x){return x * 2;}
 
-function b(x, y, a) {
-    arguments[2] = 10;
-    alert(a);
-}
-b(1, 2, 3); // выведет 10 
+// function b(x, y, a) {
+//     arguments[2] = 10;
+//     alert(a);
+// }
+// b(1, 2, 3); // выведет 10 присвоит а = 10
 
-function a() {
-    alert(this); // Выводит объект Window
-}
-a.call(null);
+// function a() {
+//     alert(this); //объект window
+// }
+// a.call(null);
